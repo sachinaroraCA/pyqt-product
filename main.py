71cc6552a5165c4a06d691f4e80bd7360e31f814
Aman preet Singh <amansinghbawa@gmail.com>
2018-12-31 15:30:12 +0530

    app commited

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sys
 from windows.login import Ui_LoginWindow
 from PyQt5 import QtWidgets
 
+import os
+
+# Directory paths
+UTIL_DIRECTORY = os.getcwd()
+PROJECT_DIRECTORY = os.path.abspath(os.pardir)
+
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_LoginWindow(MainWindow)
