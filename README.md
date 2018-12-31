3aa2ec92b8566a324332a40e9c9042af08f2aab8
Aman preet Singh <amansinghbawa@gmail.com>
2018-12-31 15:39:54 +0530

    dependencies added

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # PyQt-Product
+
+Install all the dependencies from requirement.txt file by using the command
+- pip install requirement.txt
+
 set the name of the database in database_util.py file
 
 run main.py file in windows folder
--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import sys
 from windows.login import Ui_LoginWindow
 from PyQt5 import QtWidgets
 
-import os
-
-# Directory paths
-UTIL_DIRECTORY = os.getcwd()
-PROJECT_DIRECTORY = os.path.abspath(os.pardir)
-
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_LoginWindow(MainWindow)
