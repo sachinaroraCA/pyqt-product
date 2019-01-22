ce4159aec5111c960f066cfd450086514d226157
sachinaroraCA <44459046+sachinaroraCA@users.noreply.github.com>
2019-01-22 21:22:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import sys
 
 from windows.login import Ui_LoginWindow
 from PyQt5 import QtWidgets
+from utils.window_utils import SCREEN_RESOLUTION
 
 app = QtWidgets.QApplication(sys.argv)
+resolution = app.desktop().screenGeometry()
+SCREEN_RESOLUTION.update({"width": resolution.width(), "height": resolution.height()})
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_LoginWindow(MainWindow)
 MainWindow.show()
